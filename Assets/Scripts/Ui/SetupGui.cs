740f25b5031843cdb2057ea802748ba2cfafc1bd
Platholl <cyprien.plateauholleville@gmail.com>
2020-04-24 17:09:07 +0200

    Scene creation and add setup gui.

---
+++ Assets/Scripts/Ui/SetupGui.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class SetupGui : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    void OnGUI()
+    {
+        
+    }
+}
