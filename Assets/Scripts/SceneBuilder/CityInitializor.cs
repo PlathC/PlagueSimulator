0113c880d8eb1b31def60d13cba566b3fb7eb54b
Platholl <cyprien.plateauholleville@gmail.com>
2020-04-24 23:48:06 +0200

    Add simulation scenes and appropriate building script.

--- Assets/Scripts/SceneBuilder/CityInitializor.cs
+++ Assets/Scripts/SceneBuilder/CityInitializor.cs
@@ -1,16 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class NewBehaviourScript1 : MonoBehaviour
+public class CityInitializor : MonoBehaviour
 {
+    [SerializeField]
+    private SimulationData m_simulationData = null;
 
-    // Use this for initialization
+#region PrefabsFields
+
+    [SerializeField]
+    private GameObject m_housePrefab = null;
+
+    [SerializeField]
+    private GameObject m_citizenPrefab = null;
+
+#endregion //PrefabsFields
+
     void Start()
     {
         
+        m_simulationData = ScriptableObject.FindObjectOfType<SimulationData>();
+        var map = GameObject.FindGameObjectWithTag("Map");
+
+        uint height = 0, width = 0;
+        
+        if (map.GetComponent<Renderer>() != null)
+        {
+            var size = map.GetComponent<Renderer>().bounds.size;
+            width = (uint)size.x;
+            height = (uint)size.z;
+        }
+
+        for (uint i = 0; i < m_simulationData.populationDensity; i++)
+        {
+
+            // Instantiate House prefab
+            var position = new Vector3(map.transform.position.x + Random.Range(0, width) - width / 2,
+                                       map.transform.position.y + 0,
+                                       map.transform.position.z + Random.Range(0, height) - height / 2);
+            var house = Instantiate(m_housePrefab,
+                                    position, 
+                                    Quaternion.identity);
+            house.transform.parent = map.transform;
+
+            var citizen = Instantiate(m_citizenPrefab,
+                                      position,
+                                      Quaternion.identity);
+            citizen.transform.parent = map.transform;
+        }
     }
 
-    // Update is called once per frame
     void Update()
     {
 
